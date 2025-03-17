e92c64818a85ee860018c31378926a5fd463ea29
Antti Varis <ajvari88@gmail.com>
2025-03-17 19:43:50 +0200

    create springboot project

---
+++ src/test/java/backend25/bookedin/BookedinApplicationTests.java
@@ -0,0 +1,13 @@
+package backend25.bookedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
