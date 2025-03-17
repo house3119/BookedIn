c974dc3a22f005ae30024993d605690e1c5c78c9
Antti Varis <ajvari88@gmail.com>
2025-03-17 20:57:56 +0200

    fills in db with countries from csv file

--- src/main/java/backend25/bookedin/BookedinApplication.java
+++ src/main/java/backend25/bookedin/BookedinApplication.java
@@ -1,11 +1,15 @@
 package backend25.bookedin;
 
+import java.io.FileReader;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.opencsv.CSVReader;
 
 import backend25.bookedin.model.AccountType;
 import backend25.bookedin.model.AccountTypeRepository;
@@ -15,16 +19,7 @@ import backend25.bookedin.model.CountryRepository;
 @SpringBootApplication
 public class BookedinApplication {
 
-    private final AccountTypeRepository accountTypeRepository;
-
-    private final CountryRepository countryRepository;
-
 	private static final Logger log = LoggerFactory.getLogger(BookedinApplication.class);
-
-    BookedinApplication(CountryRepository countryRepository, AccountTypeRepository accountTypeRepository) {
-        this.countryRepository = countryRepository;
-        this.accountTypeRepository = accountTypeRepository;
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookedinApplication.class, args);
@@ -41,12 +36,19 @@ public class BookedinApplication {
 			accountTypeRepository.save(new AccountType("USER", "Basic user without any extra priviledges."));
 			accountTypeRepository.save(new AccountType("ADMIN", "Admin level user. Can perform various actions that normal users can't."));
 
-			log.info("Add some countries to db...");
-			countryRepository.save(new Country("Finland"));
-			countryRepository.save(new Country("Sweden"));
-			countryRepository.save(new Country("Denmark"));
+			log.info("Add countries to db from file...");
+			try (CSVReader csvReader = new CSVReader(new FileReader("src/main/resources/static/FCDO_Geographical_Names_Index_March_2024.csv"))) {
+				String[] next;
+				while ((next = csvReader.readNext()) != null) {
+					if (next[1] != "Name") {
+						countryRepository.save(new Country(next[1]));
+					}
+				}
+			}
+
 
 		};
+
 	}
 
 }
