47deb4c6e2b34dfa00e2bd38adc7b3ce9347a2ae
Antti Varis <ajvari88@gmail.com>
2025-03-27 14:02:37 +0200

    Added some main class tests

--- src/test/java/backend25/bookedin/BookedinApplicationTests.java
+++ src/test/java/backend25/bookedin/BookedinApplicationTests.java
@@ -1,13 +1,31 @@
 package backend25.bookedin;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import backend25.bookedin.model.AppUserRepository;
 
 @SpringBootTest
 class BookedinApplicationTests {
 
+	private final AppUserRepository appUserRepository;
+
+	@Autowired
+	public BookedinApplicationTests(AppUserRepository appUserRepository) {
+		this.appUserRepository = appUserRepository;
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testDbConnection() {
+		assertThat(appUserRepository).isNotNull();
+		assertThat(appUserRepository.count()).isNotNull();
 	}
 
 }
